8d65884a04c81bdedaebc3525b78b09087859cc7
Larissa-Fabiana <ferreira-larissa@outlook.com>
2018-08-13 18:12:50 -0300

    função login

--- public/app.js
+++ public/app.js
@@ -1,8 +1,31 @@
-firebase.auth().createUserWithEmailAndPassword(email, password)
-.then()
-.catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
+$(document).ready(function(){
+
+  $(".btncreate").click(function(event){
+    event.preventDefault();
+    var createEmail = $("#createemail").val();
+    var createPassword = $("#createpassword").val();
+  
+    firebase.auth().createUserWithEmailAndPassword(createEmail, createPassword)
+    .then()
+    .catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorCode, errorMessage);
+    });
   });
+
+  $(".btnenter").click(function(event){
+    event.preventDefault();
+    var enterEmail = $("#enteremail").val();
+    var enterPassword = $("#enterpassword").val();
+
+    firebase.auth().signInWithEmailAndPassword(enterEmail, enterPassword)
+    .catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorCode, errorMessage);
+    });
+  });
+
+
+});
